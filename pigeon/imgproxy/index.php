3f0242fa4e96a8b850828239bb982104ae75551f
Akkariin Meiko <akkariins@gmail.com>
2025-02-16 04:58:37 +0800

    Refactoring imgproxy

--- pigeon/imgproxy/index.php
+++ pigeon/imgproxy/index.php
@@ -1,46 +1,62 @@
 <?php
 @require_once("../config.php");
+
 error_reporting(0);
 set_time_limit(6);
-OB_START();
 SESSION_START();
-function curl_request($url, $post = '', $cookie = '', $headers = '', $returnHeader = 0) {
-    $curl = curl_init();
-    curl_setopt($curl, CURLOPT_URL, $url);
-    curl_setopt($curl, CURLOPT_USERAGENT, 'Mozilla/5.0 (compatible; MSIE 10.0; Windows NT 6.1; Trident/6.0)');
-    curl_setopt($curl, CURLOPT_FOLLOWLOCATION, 1);
-    curl_setopt($curl, CURLOPT_AUTOREFERER, 1);
-    curl_setopt($curl, CURLOPT_REFERER, $url);
+
+// 最大缓存文件数量
+$max_cache = 30;
+// 最大缓存文件大小
+$max_cache_length = 1024 * 1024 * 10;
+// 缓存文件类型
+$cache_list = array(
+	'jpg' => true,
+	'png' => true,
+	'bmp' => true,
+	'gif' => true
+);
+
+function curl_request($url, $post = '', $cookie = '', $headers = '', $return_headers = 0)
+{
+	$curl = curl_init();
+	curl_setopt($curl, CURLOPT_URL, $url);
+	curl_setopt($curl, CURLOPT_USERAGENT, 'Mozilla/5.0 (compatible; MSIE 10.0; Windows NT 6.1; Trident/6.0)');
+	curl_setopt($curl, CURLOPT_FOLLOWLOCATION, 1);
+	curl_setopt($curl, CURLOPT_AUTOREFERER, 1);
+	curl_setopt($curl, CURLOPT_REFERER, $url);
 	curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false);
-    if ($post) {
-        curl_setopt($curl, CURLOPT_POST, 1);
-        curl_setopt($curl, CURLOPT_POSTFIELDS, http_build_query($post));
-    }
-    if ($cookie) {
-        curl_setopt($curl, CURLOPT_COOKIE, $cookie);
-    }
+	if ($post) {
+		curl_setopt($curl, CURLOPT_POST, 1);
+		curl_setopt($curl, CURLOPT_POSTFIELDS, http_build_query($post));
+	}
+	if ($cookie) {
+		curl_setopt($curl, CURLOPT_COOKIE, $cookie);
+	}
 	if ($headers) {
-        curl_setopt($curl, CURLOPT_HTTPHEADER, $headers);
-    }
-    curl_setopt($curl, CURLOPT_HEADER, 1);
-    curl_setopt($curl, CURLOPT_TIMEOUT, 5);
-    curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
-    $data = curl_exec($curl);
-    if (curl_errno($curl)) {
-        return curl_error($curl);
-    }
-    curl_close($curl);
+		curl_setopt($curl, CURLOPT_HTTPHEADER, $headers);
+	}
+	curl_setopt($curl, CURLOPT_HEADER, 1);
+	curl_setopt($curl, CURLOPT_TIMEOUT, 5);
+	curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
+	$data = curl_exec($curl);
+	if (curl_errno($curl)) {
+		return curl_error($curl);
+	}
+	curl_close($curl);
 	list($header, $body) = explode("\r\n\r\n", $data, 2);
 	$info['header'] = $header;
 	$info['body'] = $body;
 	return $info;
 }
-function unlinkdir($dir) {
-	if($handle = opendir($dir)) {
-		while(($item = readdir($handle)) !== false) {
-			if($item != "." && $item !== "..") {
-				if(is_dir("{$dir}/{$item}")) { 
-					unlinkdir("{$dir}/{$item}"); 
+
+function unlinkdir($dir)
+{
+	if ($handle = opendir($dir)) {
+		while (($item = readdir($handle)) !== false) {
+			if ($item != "." && $item !== "..") {
+				if (is_dir("{$dir}/{$item}")) {
+					unlinkdir("{$dir}/{$item}");
 				} else {
 					unlink("{$dir}/{$item}");
 				}
@@ -49,11 +65,13 @@ function unlinkdir($dir) {
 		closedir($handle);
 	}
 }
-function getFilesNum($dir) {
+
+function getFilesNum($dir)
+{
 	$i = 0;
-	if($handle = opendir($dir)) {
-		while(($item = readdir($handle)) !== false) {
-			if($item != "." && $item !== "..") {
+	if ($handle = opendir($dir)) {
+		while (($item = readdir($handle)) !== false) {
+			if ($item != "." && $item !== "..") {
 				$i++;
 			}
 		}
@@ -61,87 +79,141 @@ function getFilesNum($dir) {
 	}
 	return $i;
 }
-function decrypt($str, $localIV, $encryptKey) {
+
+function decrypt($str, $localIV, $encryptKey)
+{
 	return openssl_decrypt($str, 'AES-256-CFB', $encryptKey, 0, $localIV);
 }
-$cache_list = Array(
-	'jpg' => true,
-	'png' => true,
-	'bmp' => true,
-	'gif' => true
-);
-if(!isset($_GET['url'], $_GET['token'])) {
-	exit("403 Forbidden");
+
+function validRequest()
+{
+	if (!isset($_GET['url'], $_GET['token'])) {
+		errorImage();
+	}
+
+	$token  = $_GET['token'];
+	$url    = base64_decode($_GET['url']);
+	$parse  = parse_url($url);
+	$host   = isset($parse['host']) ? $parse['host'] : '';
+	$scheme = isset($parse['scheme']) ? $parse['scheme'] : '';
+
+	if ($token !== sha1($url . $_SESSION['seid'])) {
+		errorImage();
+	}
+
+	// 防止死循环请求
+	if (stristr($url, "imgproxy")) {
+		errorImage();
+	}
+
+	// 防止请求非法域名/内网IP
+	if (!isset($scheme) || empty($scheme)) {
+		errorImage();
+	}
+
+	// 只允许 http 和 https 协议
+	if (!in_array($scheme, array('http', 'https'))) {
+		errorImage();
+	}
+
+	// 先判断是否是 IP，如果不是就先解析域名
+	if (!filter_var($host, FILTER_VALIDATE_IP)) {
+		$host = gethostbyname($host);
+	}
+
+	// 判断是否是内网 IP
+	if (!filter_var($host, FILTER_VALIDATE_IP, FILTER_FLAG_NO_PRIV_RANGE | FILTER_FLAG_NO_RES_RANGE)) {
+		errorImage();
+	}
+
+	// 验证通过，返回 URL
+	return $url;
 }
-$url = base64_decode($_GET['url']);
-$token = $_GET['token'];
-if($token !== sha1($url . $_SESSION['seid'])) {
-	exit("403 Forbidden");
-}
-// 防止死循环请求
-if(stristr($url, "imgproxy")) {
-	exit("403 Forbidden");
-}
-// 防止请求非法域名/内网IP
-$host = parse_url($url, PHP_URL_HOST);
-if(!isset($host) || empty($host)) {
-	exit("403 Forbidden");
-}
-// 先判断是否是 IP，如果不是就先解析域名
-if(!filter_var($host, FILTER_VALIDATE_IP)) {
-	$host = gethostbyname($host);
-}
-if(!filter_var($host, FILTER_VALIDATE_IP, FILTER_FLAG_NO_PRIV_RANGE | FILTER_FLAG_NO_RES_RANGE)) {
-	exit("403 Forbidden");
-}
-// 最大缓存文件数量
-$max_cache = 30;
-// 最大缓存文件大小
-$max_cache_length = 1024 * 1024 * 10;
-$pathtype = pathinfo($url)['extension'];
-$filecache = "cache/" . date("Ymd") . "/" . urlencode(base64_encode($url));
-if(getFilesNum("cache/") > $max_cache) {
-	unlinkdir("cache/");
-}
-if(isset($cache_list[$pathtype])) {
-	if(!file_exists("cache/" . date("Ymd") . "/")) {
+
+function processCache($url)
+{
+	global $cache_list, $max_cache;
+	$pathtype = pathinfo($url)['extension'];
+	$filecache = sprintf("cache/%s/%s", date("Ymd"), urlencode(base64_encode($url)));
+	if (getFilesNum("cache/") > $max_cache) {
 		unlinkdir("cache/");
-		mkdir("cache/" . date("Ymd") . "/");
-	} else {
-		if(file_exists($filecache)) {
-			$finfo = finfo_open(FILEINFO_MIME);
-			$mimetype = finfo_file($finfo, $filecache);
-			finfo_close($finfo);
-			if(stristr($mimetype, "image/")) {
-				Header("Content-type: {$mimetype}");
-				readfile($filecache);
-			} else {
-				Header("Content-type: image/png", true, 502);
-				readfile("502.png");
-				exit;
+	}
+	if (isset($cache_list[$pathtype])) {
+		if (!file_exists("cache/" . date("Ymd") . "/")) {
+			unlinkdir("cache/");
+			mkdir("cache/" . date("Ymd") . "/");
+		} else {
+			if (file_exists($filecache)) {
+				$finfo = finfo_open(FILEINFO_MIME);
+				$mimetype = finfo_file($finfo, $filecache);
+				finfo_close($finfo);
+				if (stristr($mimetype, "image/")) {
+					Header("Content-type: {$mimetype}");
+					readfile($filecache);
+				} else {
+					errorImage();
+				}
+				return true;
 			}
-			exit;
 		}
+	}
+	return false;
+}
+
+function cacheFile($url)
+{
+	global $cache_list, $max_cache_length;
+	$pathtype  = pathinfo($url)['extension'];
+	$filecache = sprintf("cache/%s/%s", date("Ymd"), urlencode(base64_encode($url)));
+	$data = curl_request($url);
+	if (isset($data['header']) && isset($data['body'])) {
+		$body = $data['body'];
+		if (isset($cache_list[$pathtype])) {
+			if (strlen($body) < $max_cache_length) {
+				safeSaveImage($body, $filecache);
+			}
+		}
+		safeSaveImage($body);
+	} else {
+		errorImage();
 	}
 }
-$data = curl_request($url);
-if(isset($data['header']) && isset($data['body'])) {
-	$header = $data['header'];
-	$exp = explode("\r\n", $header);
-	foreach($exp as $hd) {
-		if(stristr($hd, "Content-type:") || stristr($hd, "Content-length:")) {
-			Header($hd, true);
+
+function safeSaveImage($data, $path = false)
+{
+	$img = @imagecreatefromstring($data);
+	if ($img) {
+		// resize image
+		$width = imagesx($img);
+		$height = imagesy($img);
+		$tmp = imagecreatetruecolor($width, $height);
+		// transparent background
+		imagealphablending($tmp, false);
+		imagesavealpha($tmp, true);
+		$transparent = imagecolorallocatealpha($tmp, 255, 255, 255, 127);
+		imagefilledrectangle($tmp, 0, 0, $width, $height, $transparent);
+		imagecopyresampled($tmp, $img, 0, 0, 0, 0, $width, $height, $width, $height);
+		imagedestroy($img);
+		if ($path) {
+			imagepng($tmp, $path);
+		} else {
+			header("Content-type: image/png");
+			imagepng($tmp);
 		}
+		imagedestroy($tmp);
+	} else {
+		errorImage();
 	}
-	$body = $data['body'];
-	if(isset($cache_list[$pathtype])) {
-		if(strlen($body) < $max_cache_length) {
-			@file_put_contents($filecache, $body);
-		}
-	}
-	echo $body;
-} else {
+}
+
+function errorImage()
+{
 	Header("Content-type: image/png", true, 502);
 	readfile("502.png");
 	exit;
+}
+
+$url = validRequest();
+if (!processCache($url)) {
+	cacheFile($url);
 }
